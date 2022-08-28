d7e6233e2f6c48df6a9dad422cf5534c7f525c09
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2022-08-29 03:23:44 +0800

    Update and rename jd_tuitui1.js to jd_tuitui.js

--- jd_tuitui.js
+++ jd_tuitui.js
@@ -8,7 +8,7 @@
 const $ = new Env('推推赚大钱'); 
 const notify = $.isNode() ? require("./sendNotify") : "";
  ///**中间为需要设置的参数
-const actId ="49f40d2f40b3470e8d6c39aa4866c7ff";
+const actId ="3075b6eab065464dad1c4042d345ac97";
 let n = 0; //如果只希望跑满5人团，n=0 ；如果希望跑满25人，n=1 ；n如果设置为0和1以外的数字表示无限助力
 
   //中间为需要设置的参数*/
