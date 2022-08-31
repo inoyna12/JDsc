7a5cf0bb6e372a1c764ccb9813e6880fbd4d4b72
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2022-08-31 13:48:04 +0800

    Update jx_sign.js

--- jx_sign.js
+++ jx_sign.js
@@ -59,7 +59,7 @@ if ($.isNode()) {
       cookie = cookiesArr[i];
       $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
       $.isLogin = true;
-      UA = `jdpingou;iPhone;4.13.0;14.4.2;${randomString(40)};network/wifi;model/iPhone10,2;appBuild/100609;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/${Math.random * 98 + 1};pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148`
+      UA = `jdpingou;iPhone;5.22.2;14.6;${randomString(40)};network/wifi;model/iPhone10,2;appBuild/100880;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/${Math.random * 98 + 1};pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148`
       UAInfo[$.UserName] = UA
       if (isLoginInfo[$.UserName] === false) {
       
@@ -366,16 +366,14 @@ function dotask(task) {
 
 // 宝箱
 function bxdraw() {
-  let functionId, body;
+  let body;
   if ($.signhb_source === '5') {
-    functionId = "signhb/bxdraw_jxpp"
     body = `ispp=1&sqactive=${$.sqactive}&tk=`
   } else {
-    functionId = "signhb/bxdraw"
     body = `ispp=0&sqactive=&tk=`
   }
   return new Promise((resolve) => {
-    $.get(taskUrl(functionId, body), async (err, resp, data) => {
+    $.get(taskUrl("signhb/bxdraw", body), async (err, resp, data) => {
       try {
         if (err) {
           console.log(JSON.stringify(err));
